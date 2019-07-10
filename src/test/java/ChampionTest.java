b2001e7254627b3be46df271dc0b4fe8804854bd
nahyun <nahyun858@gmail.com>
2019-07-10 21:05:04 +0900

    hasitem method

--- src/test/java/ChampionTest.java
+++ src/test/java/ChampionTest.java
@@ -168,6 +168,14 @@ public class ChampionTest {
         String bestChampion = "자르반";
         assertThat(popularChampion,hasItemInArray(bestChampion));
     }
+    //hasitem 을 통해서 collection에 아이템이 있는지 확인하는 테스트
+    @Test
+    public void shouldHaveChampionInCollection()
+    {
+        List<String> championCollection = Arrays.asList("자르반","쓰레쉬","리신","베인","이즈리얼");
+        String iwantChampion = "이즈리얼";
+        assertThat(championCollection,hasItem(iwantChampion));
+    }
 
 
 }
